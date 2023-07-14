42f1fbc2d7a31dd0972b9218dd437e0fe90c2a49
muhammaduss <muhammad20040130@gmail.com>
2023-07-15 01:07:01 +0300

    add items to chosen topic

--- bot.py
+++ bot.py
@@ -3,12 +3,12 @@ import sqlite3
 
 from telebot import types
 from config import bot
-from db import BotDatabase
 
-bot_db = BotDatabase('wishlist.db')
+connection = sqlite3.connect('wishlists.db', check_same_thread=False)
+cursor = connection.cursor()
 
+cursor.execute("CREATE TABLE IF NOT EXISTS wishlists (chat_id INTEGER, topic TEXT, item TEXT, item_number INTEGER)")
 
-# cursor.execute()
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -24,12 +24,44 @@ def start_message(message):
     bot.send_message(message.chat.id, 'What you want to do?', reply_markup=markup)
 
 
+@bot.message_handler(commands=['add'])
+def adding(message):
+    bot.send_message(message.chat.id, 'Write topic name:')
+    bot.register_next_step_handler(message, get_topic_name)
+
+
 @bot.message_handler(content_types=['text'])
 def handler(message):
     if message.text == "Create new wishlist":
-        bot.send_message(message.chat.id, 'Creating...')
+        bot.send_message(message.chat.id, 'Write name of your wishlist:', reply_markup=types.ReplyKeyboardRemove())
+
+        bot.register_next_step_handler(message, create_new_wishlist)
     elif message.text == "Open existing wishlist":
         bot.send_message(message.chat.id, 'One moment, please...')
+
+
+def create_new_wishlist(message):
+    chat_id = message.chat.id
+    topic = message.text
+    cursor.execute("INSERT INTO wishlists VALUES (?, ?, NULL, NULL)", (chat_id, topic))
+    connection.commit()
+    bot.send_message(message.chat.id, 'Congratulations, your wishlist has been added! Now you can add items or delete'
+                                      ' them using this commands: /add and /delete ')
+
+
+def get_topic_name(message):
+    topic = message.text
+    bot.send_message(message.chat.id, 'Write item which you want to add for chosen topic:')
+    bot.register_next_step_handler(message, add_item, topic)
+
+
+def add_item(message, topic):
+    chat_id = message.chat.id
+    item = message.text
+    cursor.execute("SELECT * FROM wishlists WHERE `chat_id` = ? AND `topic` = ?", (chat_id, topic))
+    item_number = len(cursor.fetchall())
+    cursor.execute("INSERT INTO wishlists VALUES (?, ?, ?, ?)", (chat_id, topic, item, item_number))
+    connection.commit()
 
 
 bot.infinity_polling()
