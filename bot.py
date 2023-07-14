d52c1b87486fe631e7fdc2d005b8e78d5a7b06b1
muhammaduss <muhammad20040130@gmail.com>
2023-07-15 01:47:47 +0300

    delete functionality

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import telebot
 import sqlite3
 
 from telebot import types
@@ -25,9 +24,19 @@ def start_message(message):
 
 
 @bot.message_handler(commands=['add'])
-def adding(message):
+def add(message):
     bot.send_message(message.chat.id, 'Write topic name:')
-    bot.register_next_step_handler(message, get_topic_name)
+    message_for_user = 'Write item which you want to add to chosen topic:'
+    operation = 'add'
+    bot.register_next_step_handler(message, get_topic_name, message_for_user, operation)
+
+
+@bot.message_handler(commands=['delete'])
+def delete(message):
+    bot.send_message(message.chat.id, 'Write topic name:')
+    message_for_user = 'Write number of item which you want to delete from chosen topic:'
+    operation = 'delete'
+    bot.register_next_step_handler(message, get_topic_name, message_for_user, operation)
 
 
 @bot.message_handler(content_types=['text'])
@@ -49,10 +58,13 @@ def create_new_wishlist(message):
                                       ' them using this commands: /add and /delete ')
 
 
-def get_topic_name(message):
+def get_topic_name(message, message_for_user, operation):
     topic = message.text
-    bot.send_message(message.chat.id, 'Write item which you want to add for chosen topic:')
-    bot.register_next_step_handler(message, add_item, topic)
+    bot.send_message(message.chat.id, message_for_user)
+    if operation == 'add':
+        bot.register_next_step_handler(message, add_item, topic)
+    else:
+        bot.register_next_step_handler(message, delete_item, topic)
 
 
 def add_item(message, topic):
@@ -61,6 +73,21 @@ def add_item(message, topic):
     cursor.execute("SELECT * FROM wishlists WHERE `chat_id` = ? AND `topic` = ?", (chat_id, topic))
     item_number = len(cursor.fetchall())
     cursor.execute("INSERT INTO wishlists VALUES (?, ?, ?, ?)", (chat_id, topic, item, item_number))
+    connection.commit()
+
+
+def delete_item(message, topic):
+    chat_id = message.chat.id
+    item_number = int(message.text)
+
+    cursor.execute("DELETE FROM wishlists WHERE `chat_id` = ? AND `topic` = ? AND `item_number` = ?",
+                   (chat_id, topic, item_number))
+    connection.commit()
+
+    cursor.execute("SELECT MAX(`item_number`) FROM wishlists WHERE `chat_id` = ? AND `topic` = ?", (chat_id, topic))
+    max_item_number = cursor.fetchone()[0]
+    cursor.execute("UPDATE wishlists SET `item_number` = `item_number` - 1 WHERE `item_number` BETWEEN ? AND ?",
+                   (item_number + 1, max_item_number))
     connection.commit()
 
 
